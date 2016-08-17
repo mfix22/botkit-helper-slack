c325895c9f6ed08470ceb9bcfc79f640360568cb
Michael Fix <mfix@wisc.edu>
2016-08-17 10:41:40 -0500

    update v0.0.1 for production

---
+++ src/index.js
@@ -0,0 +1,128 @@
+// .------..------..------..------..------.     .------..------..------..------..------.
+// |S.--. ||L.--. ||A.--. ||C.--. ||K.--. |.-.  |S.--. ||T.--. ||Y.--. ||L.--. ||E.--. |
+// | :/\: || :/\: || (\/) || :/\: || :/\: ((5)) | :/\: || :/\: || (\/) || :/\: || (\/) |
+// | :\/: || (__) || :\/: || :\/: || :\/: |'-.-.| :\/: || (__) || :\/: || (__) || :\/: |
+// | '--'S|| '--'L|| '--'A|| '--'C|| '--'K| ((1)) '--'S|| '--'T|| '--'Y|| '--'L|| '--'E|
+// `------'`------'`------'`------'`------'  '-'`------'`------'`------'`------'`------'
+
+function _capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function get(key) {
+  var helperStrings = {
+    bullet : '•',
+    here : '<!here|here>',
+    channel : '<!channel>',
+    group : '<!group>',
+    everyone : "<!everyone>"
+  }
+  return helperStrings[key];
+}
+
+var __functions = {
+  // emphasis
+  italic : function() {
+    return new SS("_" + this.s + "_").s;
+  },
+  bold : function() {
+    return new SS("*" + this.s + "*").s;
+  },
+  code : function() {
+    return new SS("`" + this.s + "`").s;
+  },
+  pre : function() {
+    return new SS("```" + this.s + "```").s;
+  },
+  strike : function() {
+    return new SS("~" + this.s + "~").s;
+  },
+  quote : function() {
+    return new SS("> " + this.s).s;
+  },
+  paragraph : function() {
+    return new SS('>>> ' + this.s).s;
+  },
+  emoji : function () {
+    return new SS(":" + this.s.toLowerCase().split(' ').join('_').trim() + ":").s;
+  },
+
+  // types of selectors
+  //e.g. <@U123456>
+  user : function() {
+    return new SS("<@" + this.s.trim() + ">").s;
+  },
+  // basically a alias for user
+  channel : function() {
+    return new SS("<@" + this.s.trim() + ">").s;
+  },
+  //e.g. <!subteam^123456|sub-team>
+  subteam : function (handle) {
+    if (!handle) throw Error('Parameter "handle" is required for function subteam().')
+    return new SS('<!subteam^' + this.s.trim() + '|' + handle + '>').s;
+  },
+
+  // links
+  url : function(handle) {
+    if (handle) return new SS("<" + this.s.trim() + '|' + handle + ">").s;
+    return new SS("<" + this.s.trim() + ">").s;
+  },
+  //e.g. <mailto:test@email.com|test>
+  email : function (handle) {
+    if (handle) return new SS("<mailto:" + this.s.trim() + '|' + handle + ">").s;
+    return new SS("<mailto:" + this.s.trim() + ">").s;
+  },
+
+  // variables
+  atHere : function() {
+    return get('here');
+  },
+  atEveryone : function() {
+    return get('everyone');
+  },
+  atGroup : function() {
+    return get('group');
+  },
+  atChannel : function() {
+    return get('channel');
+  },
+
+  html : function() {
+    return new SS(this.s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')).s;
+  }
+}
+Object.freeze(__functions)
+
+
+function extendPrototype() {
+  for (var name in __functions) {
+    (function(name){
+      var func = __functions[name];
+        var name = 'slack' + _capitalize(name);
+        if (!String.prototype.hasOwnProperty(name)) {
+          String.prototype[name] = function() {
+            String.prototype.s = this;
+            return func.apply(this, arguments);
+          }
+        }
+    })(name);
+  }
+  return this;
+}
+
+// SS = 'SLACK STYLE'
+function SS(s) {
+  this.s = s;
+}
+// SS('my').italic() = '_my_'
+for (var i in __functions) {
+  if (__functions.hasOwnProperty(i)) {
+    SS.prototype[i] = __functions[i];
+  }
+}
+
+module.exports = function(s) {
+  return new SS(s);
+}
+module.exports.extendPrototype = extendPrototype;
+module.exports.get = get;
